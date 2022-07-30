a1fb3d1139c96204848250bd560afbb33202221b
Miguel Pousa <miguelpousafreire@gmail.com>
2022-07-30 10:06:04 +0200

    adding utilities

--- bughunt/utils.py
+++ bughunt/utils.py
@@ -1,5 +1,27 @@
 """Module."""
 import logging
+from dataclasses import dataclass
+
+
+class State:
+    """State."""
+
+    # TODO serialize this class
+    # TODO deserialize this class
+    ...
+
+
+@dataclass
+class SpriteState:
+    """Sprite State."""
+
+    rotation: float = 0.
+    x: int = 0
+    y: int = 0
+
+    def as_tuple(self):
+        """As tuple."""
+        return (self.x, self.y, self.rotation)
 
 
 def main():
