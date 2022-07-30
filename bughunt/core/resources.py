5b56124e6da163e8dbb081297f72e71eb4da07cd
BlockOG <alesiobaca@gmail.com>
2022-07-30 18:45:57 +0300

    Is this code in resources.py necessary? And removed wsproto

--- bughunt/core/resources.py
+++ bughunt/core/resources.py
@@ -1,7 +1,4 @@
 """Resources."""
-import logging
-import os
-
 import pyglet
 
 
@@ -12,26 +9,10 @@ def center_image(image):
 
 
 # Tell pyglet where to find the resources
-print(pyglet.resource._default_loader._script_home)
-print(pyglet.resource._default_loader._index)
 pyglet.resource.path = ['..\\resources', '.\\resources']
 pyglet.resource.reindex()
-print(pyglet.resource._default_loader._index)
 # Load the three main resources and get them to draw centered
 player_image = pyglet.resource.image("player.png")
 maze_img = pyglet.resource.image('maze.png')
 center_image(player_image)
 center_image(maze_img)
-
-
-def main():
-    """Main function."""
-    logging.basicConfig(level=logging.INFO)
-    logging.info("Main.")
-    pyglet.resource.path = ['.\\pygletexample\\resources']
-    pyglet.resource.reindex()
-    os.walk(pyglet.resource.path)
-
-
-if __name__ == "__main__":
-    main()
