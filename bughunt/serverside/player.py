972cd73675bc95a2241fd3cf46b862f9207d4d44
Miguel Pousa <miguelpousafreire@gmail.com>
2022-07-31 18:11:59 +0200

    WIP Player simpilication

--- bughunt/serverside/player.py
+++ bughunt/serverside/player.py
@@ -3,15 +3,27 @@ import logging
 
 from pyglet.window import key
 
+from bughunt.core.resources import Map
+
 
 class PlayerServer:
     """Physical object that responds to user input"""
 
-    def __init__(self, x: float = 0., y: float = 0.):
+    def __init__(
+        self,
+        x: float = 0.,
+        y: float = 0.,
+        width: float = 0.,
+        height: float = 0.,
+        map: Map = None
+    ):
         # Set some easy-to-tweak constants
         self.thrust = 300.0
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
+        self.map = map
 
     def update(self, dt, actions=None):
         """Update the player."""
@@ -27,6 +39,17 @@ class PlayerServer:
         # we have a child sprite which myst be deleted when this object
         # is deleted from batches, etc.
         super(PlayerServer, self).delete()
+
+    def check_collisions_walls(self, x, y):
+        """Check for collisions with walls."""
+        if self.x < 0:
+            self.x = 0
+        if self.y < 0:
+            self.y = 0
+        if self.x > self.map.width - self.width:
+            self.x = self.map.width - self.width
+        if self.y > self.map.height - self.height:
+            self.y = self.map.height - self.height
 
 
 def main():
