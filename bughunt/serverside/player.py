f5c5ec79ce8df9765d8d15458b34fa1dbc9da7a8
Miguel Pousa <miguelpousafreire@gmail.com>
2022-07-31 18:01:59 +0200

    Simplify PlayerServer

--- bughunt/serverside/player.py
+++ bughunt/serverside/player.py
@@ -3,33 +3,22 @@ import logging
 
 from pyglet.window import key
 
-from bughunt.core.resources import Resources
-from bughunt.engine import physicalobject
 
-
-class PlayerServer(physicalobject.PhysicalObject):
+class PlayerServer:
     """Physical object that responds to user input"""
 
-    def __init__(self, *args, **kwargs):
-        super(PlayerServer, self).__init__(img=Resources().player_image(), *args, **kwargs)
-
+    def __init__(self, x: float = 0., y: float = 0.):
         # Set some easy-to-tweak constants
         self.thrust = 300.0
-        self.rotate_speed = 200.0
-        self.bullet_speed = 700.0
-
-        # Player should not collide with own bullets
-        self.reacts_to_bullets = False
-
-        # Let pyglet handle keyboard events for us
-        # self.key_handler = key.KeyStateHandler()
-        # self.event_handlers = [self, self.key_handler]
+        self.x = x
+        self.y = y
 
     def update(self, dt, actions=None):
         """Update the player."""
         # Do all the normal physics stuff
         super(PlayerServer, self).update(dt)
-
+        # check for collisions with walls
+        self.check_collisions_walls()
         if key.X in actions:
             self.delete()
 
